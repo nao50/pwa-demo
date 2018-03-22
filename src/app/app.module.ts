cac8ac34cf942d1b3b1bc7cd233998a741fba701
naoyamaguchi <naoyamaguchi0128@gmail.com>
2018-03-22 11:40:27 +0900

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
-
 import { environment } from '../environments/environment';
+import { RouterModule } from '@angular/router';
+
+// Material
+// import 'hammerjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatProgressBarModule,
+} from '@angular/material';
+
+// Component
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    RouterModule,
+    // material
+    BrowserAnimationsModule,
+    MatProgressBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.server.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AppShellComponent } from './app-shell/app-shell.component';
+
+// material
+import { MatProgressBarModule } from '@angular/material';
+
+const routes: Routes = [ { path: 'shell', component: AppShellComponent }];
+
+@NgModule({
+  imports: [
+    AppModule,
+    ServerModule,
+    RouterModule.forRoot(routes),
+    MatProgressBarModule,
+  ],
+  bootstrap: [AppComponent],
+  declarations: [AppShellComponent],
+})
+export class AppServerModule {}
